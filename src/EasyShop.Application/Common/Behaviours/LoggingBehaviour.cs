77640c98665a017ca873b371e932f0e6e98a1f21
abolfazlafshar <abolfazlafshar003@gmail.com>
2026-02-13 23:39:44 +0330

    add behaviour for mediator

---
+++ src/EasyShop.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,26 @@
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace EasyShop.Application.Common.Behaviours;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    where TRequest : notnull
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        //TODO current user to enrich log
+        
+        _logger.LogInformation("easyShop Request: {Name} {@Request}",
+            requestName, request);
+        
+    }
+}
